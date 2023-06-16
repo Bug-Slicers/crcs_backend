65304a9c3eede9e8321c76f35a9e6b66f1f4fff0
manasjadhav <manasjjadhav@gmail.com>
2023-06-16 17:48:57 +0530

    done with registedSocietyRoute

--- routers/society/society.router.js
+++ routers/society/society.router.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { test, society_signup } = require("../../controllers/society/society.controller");
+const { test, society_signup, getRegisteredSocieties } = require("../../controllers/society/society.controller");
 const societyRouter = Router();
 
 societyRouter.get("/test", test)
+societyRouter.get("/registered-societies", getRegisteredSocieties);
 societyRouter.post("/signup", society_signup);
 
 module.exports = societyRouter;
